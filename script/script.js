3fd61f082305a945015f3016911c9981b7ea0739
Igor Arnaut <48650366+igor722@users.noreply.github.com>
2023-09-13 16:17:11 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,34 @@
+//TABS
+
+function openPage(pageName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+function myFunction() {
+  document.getElementById("defaultOpen").click();
+}
+
+//console.log("hey");
+
+//document.getElementById("defaultOpen").click();
+//document.querySelectorAll(".defaultOpen").click();
+
+//CAROUSEL
